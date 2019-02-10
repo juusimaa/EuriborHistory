e5defd5abfc167b508bf71d836af38b5650af37b
Jouni Uusimaa <jouni.uusimaa@gmail.com>
2019-02-10 15:23:15 +0200

    Initial commit.

---
+++ MvvmLight1/ViewModel/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MvvmLight1.Model;
+using System;
+using System.Net;
+using System.Windows.Input;
+
+namespace MvvmLight1.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+        private bool _isDownloading;
+
+        /// <summary>
+        /// The <see cref="WelcomeTitle" /> property's name.
+        /// </summary>
+        public const string WelcomeTitlePropertyName = "WelcomeTitle";
+
+        private string _welcomeTitle = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string WelcomeTitle
+        {
+            get
+            {
+                return _welcomeTitle;
+            }
+            set
+            {
+                Set(ref _welcomeTitle, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+
+                    WelcomeTitle = item.Title;
+                });
+
+            DownloadCommand = new RelayCommand(ExecuteDownloadCommand, CanExecuteDownloadCommand);
+        }
+
+        private bool CanExecuteDownloadCommand()
+        {
+            return !_isDownloading;
+        }
+
+        private async void ExecuteDownloadCommand()
+        {
+            _isDownloading = true;
+            var webClient = new WebClient();
+
+            await webClient.DownloadFileTaskAsync(@"https://www.emmi-benchmarks.eu/assets/modules/rateisblue/file_processing/publication/processed/hist_EURIBOR_2019.csv",
+                "2019.csv");
+
+            _isDownloading = false;
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+
+        public ICommand DownloadCommand { get; }
+    }
+}
