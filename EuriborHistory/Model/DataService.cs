e4b9849f1b82654d52fad1d9f814246855bfbe1b
Jouni Uusimaa <jouni.uusimaa@gmail.com>
2019-02-17 12:20:48 +0200

    Older XLS parsing.

--- EuriborHistory/Model/DataService.cs
+++ EuriborHistory/Model/DataService.cs
@@ -29,8 +29,8 @@ namespace EuriborHistory.Model
             "\\Euribor History";
         private readonly string[] _fileNames =
         {
-            "2004.xls",
-            "2005.xls",
+            //"2004.xls",
+            //"2005.xls",
             "2006.xls",
             "2007.xls",
             "2008.xls",
@@ -58,64 +58,157 @@ namespace EuriborHistory.Model
 
         private void ParseXls(string filename)
         {
+            var year = ParseYearFromFilename(filename);
+            if (!year.HasValue)
+            {
+                return;
+            }
+
+            if (year < 2007)
+            {
+                ParseOldXls(filename, year.Value);
+            }
+            else
+            {
+                ParseNewXls(filename);
+            }
+        }
+
+        private void ParseNewXls(string filename)
+        {
             HSSFWorkbook hssfwb;
             using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
             {
                 hssfwb = new HSSFWorkbook(file);
             }
 
-            if(hssfwb.GetSheet("Euribor") is ISheet sheet)
+            if (hssfwb.GetSheet("Euribor") is ISheet sheet)
             {
                 // 1st row is dates
                 var datesRow = sheet.GetRow(0);
                 var dates = new List<DateTime>();
-                foreach(var cell in datesRow.Cells)
+                foreach (var cell in datesRow.Cells)
                 {
                     dates.Add(cell.DateCellValue);
                 }
 
                 // rest are rates
-                for(var row = 1; row <= sheet.LastRowNum; row++)
+                for (var row = 1; row <= sheet.LastRowNum; row++)
                 {
                     var currentRow = sheet.GetRow(row);
                     var rates = new List<double>();
 
                     // TODO: dates and rates should have same count
 
-                    if(currentRow.Cells[0].CellType != CellType.String)
+                    if (currentRow.Cells[0].CellType != CellType.String)
                     {
                         break;
                     }
 
-                    switch(ParsePeriod(currentRow.Cells[0].StringCellValue))
+                    switch (ParsePeriod(currentRow.Cells[0].StringCellValue))
                     {
                         case EuriborPeriod.OneWeek:
                             ParseRow(currentRow, rates);
-                            _data.AddRange(ParseData(dates, rates, EuriborPeriod.OneWeek));
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.OneWeek));
                             break;
 
                         case EuriborPeriod.OneMonth:
                             ParseRow(currentRow, rates);
-                            _data.AddRange(ParseData(dates, rates, EuriborPeriod.OneMonth));
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.OneMonth));
                             break;
 
                         case EuriborPeriod.ThreeMonths:
                             ParseRow(currentRow, rates);
-                            _data.AddRange(ParseData(dates, rates, EuriborPeriod.ThreeMonths));
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.ThreeMonths));
                             break;
 
                         case EuriborPeriod.SixMonths:
                             ParseRow(currentRow, rates);
-                            _data.AddRange(ParseData(dates, rates, EuriborPeriod.SixMonths));
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.SixMonths));
                             break;
 
                         case EuriborPeriod.TwelveMonths:
                             ParseRow(currentRow, rates);
-                            _data.AddRange(ParseData(dates, rates, EuriborPeriod.TwelveMonths));
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.TwelveMonths));
                             break;
                     }
                 }
             }
+        }
+
+        private void ParseOldXls(string filename, int year)
+        {
+            HSSFWorkbook hssfwb;
+            using(var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                hssfwb = new HSSFWorkbook(file);
+            }
+
+            if(hssfwb.GetSheet($"hist_EURIBOR_{year}") is ISheet sheet)
+            {
+                // 1st column is dates
+                var dates = new List<DateTime>();
+                var enUS = new CultureInfo("en-US");
+
+                for (var row = 2; row <= sheet.LastRowNum; row++)
+                {
+                    var str = sheet.GetRow(row).Cells[0].ToString();
+                    if (DateTime.TryParseExact(str, "MM/dd/yy", enUS, DateTimeStyles.None, out var d))
+                    {
+                        dates.Add(d);
+                    }
+                }
+                dates = dates.OrderBy(d => d.Date).ToList();
+
+                // 1st row is periods
+                var firstRow = sheet.GetRow(0);
+                foreach (var cell in firstRow.Cells)
+                {
+                    var rates = new List<double>();
+                    switch (ParsePeriod(cell.StringCellValue))
+                    {
+                        case EuriborPeriod.OneWeek:                            
+                            for(var row = 2; row <= sheet.LastRowNum; row++)
+                            {
+                                var dataFormatter = new DataFormatter(CultureInfo.InvariantCulture);
+                                var c = sheet.GetRow(row).Cells[cell.ColumnIndex];
+
+                                if(double.TryParse(dataFormatter.FormatCellValue(c), out var d))
+                                {
+                                    rates.Add(d);
+                                }
+                            }
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.OneWeek));
+                            break;
+
+                        case EuriborPeriod.OneMonth:
+                              for (var row = 2; row <= sheet.LastRowNum; row++)
+                            {
+                                var dataFormatter = new DataFormatter(CultureInfo.InvariantCulture);
+                                var c = sheet.GetRow(row).Cells[cell.ColumnIndex];
+
+                                if (double.TryParse(dataFormatter.FormatCellValue(c), out var d))
+                                {
+                                    rates.Add(d);
+                                }
+                            }
+                            _data.AddRange(GetDataList(dates, rates, EuriborPeriod.OneMonth));
+                            break;
+                    }
+                }
+            }
+        }
+
+        private int? ParseYearFromFilename(string filename)
+        {
+            var result = new string(filename.Where(char.IsDigit).ToArray());
+
+            if (int.TryParse(result, out var i))
+            {
+                return i;
+            }
+
+            return null;
         }
 
         private static void ParseRow(IRow r, List<double> rates)
@@ -149,31 +242,31 @@ namespace EuriborHistory.Model
                     switch(ParsePeriod(set[0]))
                     {
                         case EuriborPeriod.OneWeek:
-                            _data.AddRange(ParseData(dates,
+                            _data.AddRange(GetDataList(dates,
                                                      set,
                                                      EuriborPeriod.OneWeek));
                             break;
 
                         case EuriborPeriod.OneMonth:
-                            _data.AddRange(ParseData(dates,
+                            _data.AddRange(GetDataList(dates,
                                                      set,
                                                      EuriborPeriod.OneMonth));
                             break;
 
                         case EuriborPeriod.ThreeMonths:
-                            _data.AddRange(ParseData(dates,
+                            _data.AddRange(GetDataList(dates,
                                                      set,
                                                      EuriborPeriod.ThreeMonths));
                             break;
 
                         case EuriborPeriod.SixMonths:
-                            _data.AddRange(ParseData(dates,
+                            _data.AddRange(GetDataList(dates,
                                                      set,
                                                      EuriborPeriod.SixMonths));
                             break;
 
                         case EuriborPeriod.TwelveMonths:
-                            _data.AddRange(ParseData(dates,
+                            _data.AddRange(GetDataList(dates,
                                                      set,
                                                      EuriborPeriod.TwelveMonths));
                             break;
@@ -182,7 +275,7 @@ namespace EuriborHistory.Model
             }
         }
 
-        private IEnumerable<DataItem> ParseData(IEnumerable<DateTime> dates, IEnumerable<double> values, EuriborPeriod period)
+        private IEnumerable<DataItem> GetDataList(IEnumerable<DateTime> dates, IEnumerable<double> values, EuriborPeriod period)
         {
             var results = new List<DataItem>();
 
@@ -200,7 +293,7 @@ namespace EuriborHistory.Model
             return results;
         }
 
-        private IEnumerable<DataItem> ParseData(string[] dates, string[] values, EuriborPeriod period)
+        private IEnumerable<DataItem> GetDataList(string[] dates, string[] values, EuriborPeriod period)
         {
             var results = new List<DataItem>();
 
@@ -239,23 +332,23 @@ namespace EuriborHistory.Model
 
         private EuriborPeriod ParsePeriod(string str)
         {
-            if(str.StartsWith("1w"))
+            if(str.StartsWith("1w", StringComparison.InvariantCultureIgnoreCase))
             {
                 return EuriborPeriod.OneWeek;
             }
-            if(str.StartsWith("1m"))
+            if(str.StartsWith("1m", StringComparison.InvariantCultureIgnoreCase))
             {
                 return EuriborPeriod.OneMonth;
             }
-            if(str.StartsWith("3m"))
+            if(str.StartsWith("3m", StringComparison.InvariantCultureIgnoreCase))
             {
                 return EuriborPeriod.ThreeMonths;
             }
-            if(str.StartsWith("6m"))
+            if(str.StartsWith("6m", StringComparison.InvariantCultureIgnoreCase))
             {
                 return EuriborPeriod.SixMonths;
             }
-            if(str.StartsWith("12m"))
+            if(str.StartsWith("12m", StringComparison.InvariantCultureIgnoreCase))
             {
                 return EuriborPeriod.TwelveMonths;
             }
